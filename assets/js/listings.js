ce08736be19ce3b8f39c8a89524f50aeaf97e863
baiperealestate <baiperealestate@gmail.com>
2025-12-18 21:16:02 -0400

    Update listings.js

--- assets/js/listings.js
+++ assets/js/listings.js
@@ -1,1 +1,16 @@
+fetch('data/listings.json')
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById('listings');
+    data.forEach(property => {
+      container.innerHTML += `
+        <div class="property-card">
+          <h3>${property.title}</h3>
+          <p>${property.location}</p>
+          <strong>${property.price}</strong>
+          <a href="listing.html?id=${property.id}">View Details</a>
+        </div>
+      `;
+    });
+  });
 
