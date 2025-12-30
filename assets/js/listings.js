22e83eaff793dbf8189a2c00c69005dc24f99ca0
baiperealestate <baiperealestate@gmail.com>
2025-12-30 12:44:26 -0400

    Update listings.js

--- assets/js/listings.js
+++ assets/js/listings.js
@@ -1,23 +1,21 @@
-fetch("./data/listings.json")
-  .then(response => response.json())
-  .then(listings => {
+fetch("data/listings.json")
+  .then(res => res.json())
+  .then(data => {
     const container = document.getElementById("listings");
-    container.innerHTML = "";
 
-    listings.forEach(item => {
+    data.forEach(listing => {
       container.innerHTML += `
         <div class="listing-card">
-          <img src="${item.images[0]}" alt="${item.title}">
-          <div class="listing-content">
-            <h3>${item.title}</h3>
-            <p class="price">${item.price}</p>
-            <p>${item.location}</p>
-            <p>${item.bedrooms} Beds • ${item.bathrooms} Baths • ${item.size}</p>
+          <img src="${listing.images[0]}" alt="${listing.title}">
+          <div class="listing-info">
+            <h3>${listing.title}</h3>
+            <p class="price">${listing.price}</p>
+            <p>${listing.location}</p>
+            <a href="listing.html?id=${listing.id}" class="btn">
+              View Details
+            </a>
           </div>
         </div>
       `;
     });
-  })
-  .catch(error => {
-    console.error("Listings error:", error);
   });
