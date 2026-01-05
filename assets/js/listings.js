2ec129cff39cdc837bb0206b1e42673ccc6cb930
baiperealestate <baiperealestate@gmail.com>
2026-01-04 21:33:26 -0400

    Update listings.js

--- assets/js/listings.js
+++ assets/js/listings.js
@@ -1,22 +1,20 @@
 fetch("assets/data/listings.json")
-  .then(res => res.json())
-  .then(properties => {
-    const container = document.getElementById("listings");
+  .then(response => response.json())
+  .then(data => {
+    const listings = document.getElementById("listings");
 
-    properties.forEach(p => {
-      container.innerHTML += `
-        <a href="property.html?id=${p.id}" class="listing-card">
-          <div class="listing-image">
-            <img src="${p.images[0]}" alt="${p.title}">
-          </div>
+    data.forEach(property => {
+      listings.innerHTML += `
+        <a href="property.html?id=${property.id}" class="listing-card">
+          <img src="${property.images[0]}" alt="${property.title}">
           <div class="listing-info">
-            <h3>${p.title}</h3>
-            <p class="price">${p.price}</p>
-            <p class="location">${p.location}</p>
+            <h3>${property.title}</h3>
+            <p class="price">${property.price}</p>
+            <p class="location">${property.location}</p>
             <span class="view-btn">View Details</span>
           </div>
         </a>
       `;
     });
   })
-  .catch(err => console.error("Listings error:", err));
+  .catch(err => console.error("Listings load error:", err));
