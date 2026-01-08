be7d867a7a67b876527732e55c7e982ecd9ce579
baiperealestate <baiperealestate@gmail.com>
2026-01-07 23:32:07 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
   const navLinks = document.querySelector(".nav-links");
 
-  if (hamburger && navLinks) {
+  if if (!hamburger || !nav) return; {
     hamburger.addEventListener("click", () => {
       navLinks.classList.toggle("active");
       hamburger.classList.toggle("open");
