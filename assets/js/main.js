5947123e951ff76791925ab95cef109b86cc9909
baiperealestate <baiperealestate@gmail.com>
2026-03-17 14:12:07 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -575,3 +575,19 @@ const details = document.getElementById("journeyDetails");
 details.style.display = "none";
 
 }
+
+/* ===============================
+   SCROLL ANIMATION
+=============================== */
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, { threshold: 0.15 });
+
+document.querySelectorAll(".fade-up").forEach(el => {
+  observer.observe(el);
+});
