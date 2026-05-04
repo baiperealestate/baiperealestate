2fbee817baa48dd018407fe91e5680969cc5bbfb
baiperealestate <baiperealestate@gmail.com>
2026-05-04 17:50:33 -0400

    Update team.js

--- assets/js/team.js
+++ assets/js/team.js
@@ -8,9 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
       team.forEach(member => {
         const card = document.createElement("div");
         card.className = "team-card";
-
         card.innerHTML = `
-          <a href="team-member.html?id=${member.id}" class="team-link">
+        
+        const isNL = window.location.pathname.startsWith("/nl/");
+        const memberUrl = isNL
+  ? `/nl/team-member.html?id=${member.id}`
+  : `/team-member.html?id=${member.id}`;
+<a href="${memberUrl}" class="team-link">
+  
             <div class="team-photo">
               <img src="${member.image}" alt="${member.name}">
               <span>View Profile</span>
